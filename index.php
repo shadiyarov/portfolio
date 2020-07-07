9cec23b4fdbaf224e08262cee6ce680c5152eb74
shadiyarov <sukhrobshadiyarov@Sukhrobs-MBP.home>
2020-07-07 11:49:55 -0400

    saving my code first time

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to My Portfolio</title>
+</head>
+<body>
+<h1>Welcome to Sukhrob Shadiyarov's Portfolio!!</h1>
+<?php echo "Hello world!!!" ?>
+</body>
+</html>
