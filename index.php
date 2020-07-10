6c122fd6baa72dfd191cce5f222035cf188a10f5
shadiyarov <sukhrobshadiyarov@Sukhrobs-MBP.home>
2020-07-10 12:28:01 -0400

    my calculator

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html>
 <head>
 <title>Welcome to My Portfolio</title>
+
 </head>
 <body>
 <h1>Welcome to Sukhrob Shadiyarov's Portfolio!!</h1>
 <?php echo "Hello world!!!" ?>
 </body>
 </html>
+
+<style>
+    body{
+        background-color: #1d9650;
+    }
+</style>
